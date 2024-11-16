c0776960ca1b020a32343a0daf14ad22ffb56ff9
Swagato Bhattacharya <swagatobhattacharjee7@gmail.com>
2024-11-16 21:44:50 +0530

    Update gemini_utility.py

--- gemini_utility.py
+++ gemini_utility.py
@@ -7,39 +7,40 @@ import google.generativeai as genai
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Path of config_data file
-config_file_path = f"{working_dir}/config.json"
+config_file_path = os.path.join(working_dir, "config.json")
 with open(config_file_path) as f:
     config_data = json.load(f)
 
-# Loading the GOOGLE_API_KEY
+# Load the Google API Key
 GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
 
-# Configuring google.generativeai with API key
+# Configure google.generativeai with API key
 genai.configure(api_key=GOOGLE_API_KEY)
 
+# Load Swag AI model
 def load_swag_ai_model():
     swag_ai_model = genai.GenerativeModel("gemini-pro")
     return swag_ai_model
 
-# Get response from Swag AI Vision model - image/text to text
+# Vision response for image captioning
 def swag_ai_vision_response(prompt, image):
     swag_ai_vision_model = genai.GenerativeModel("gemini-1.5-flash")
     response = swag_ai_vision_model.generate_content([prompt, image])
-    result = response.text
-    return result
+    return response.text
 
-# Get response from embeddings model - text to embeddings
+# Text embeddings response
 def swag_ai_embeddings_response(input_text):
     embedding_model = "models/embedding-001"
-    embedding = genai.embed_content(model=embedding_model,
-                                    content=input_text,
-                                    task_type="retrieval_document")
-    embedding_list = embedding["embedding"]
-    return embedding_list
+    embedding = genai.embed_content(
+        model=embedding_model,
+        content=input_text,
+        task_type="retrieval_document"
+    )
+    return embedding["embedding"]
 
-# Get response from Swag AI model - text to text
+# General text-to-text response
 def swag_ai_response(user_prompt):
     swag_ai_model = genai.GenerativeModel("gemini-pro")
     response = swag_ai_model.generate_content(user_prompt)
-    result = response.text
-    return result
+    return response.text
+
